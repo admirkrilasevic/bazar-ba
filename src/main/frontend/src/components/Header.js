8f4ae1853accb6e82208ca2e3723046f1ad51b0c
admirkrilasevic <admirkrilasevic@gmail.com>
2022-04-08 00:54:01 +0200

    switch icon on login change

--- src/main/frontend/src/components/Header.js
+++ src/main/frontend/src/components/Header.js
@@ -3,6 +3,7 @@ import styles from './Header.module.css';
 import SearchBar from './SearchBar';
 import { BsCart4 as Cart } from 'react-icons/bs';
 import { FaUser as Account } from 'react-icons/fa';
+import { ImEnter as SignIn } from 'react-icons/im';
 import AuthService from '../utils/AuthService';
 
 function Header() {
@@ -15,7 +16,7 @@ function Header() {
             <NavLink to={"/shop"} className={styles.headerLinks} activeStyle={{color: '#852400'}}>Shop</NavLink>
             <SearchBar />
             <NavLink to={"/cart"} className={styles.headerLinks} activeStyle={{color: '#852400'}}><Cart className={styles.icons}/></NavLink>
-            <NavLink to={loggedIn ? "/account" : "/login"} className={styles.headerLinks} activeStyle={{color: '#852400'}}><Account className={styles.icons}/></NavLink>
+            <NavLink to={loggedIn ? "/account" : "/login"} className={styles.headerLinks} activeStyle={{color: '#852400'}}>{loggedIn ? <Account className={styles.icons}/> : <SignIn className={styles.icons} />}</NavLink>
         </div>
     );
 }
