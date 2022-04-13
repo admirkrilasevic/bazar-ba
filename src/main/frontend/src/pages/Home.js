9ab948648161f8c3f1457d52ef3a447751eed418
admirkrilasevic <admirkrilasevic@gmail.com>
2022-04-13 21:15:04 +0200

    become a seller section

--- src/main/frontend/src/pages/Home.js
+++ src/main/frontend/src/pages/Home.js
@@ -2,6 +2,8 @@ import styles from './Home.module.css';
 import {ReactComponent as ReactLogo} from './Logo.svg';
 import {ReactComponent as HeroImage} from './Handmade.svg';
 import InfiniteScrollComponent from '../components/homePage/InfiniteScrollComponent';
+import { Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Home(){
     return(
@@ -13,6 +15,17 @@ function Home(){
             <div className={styles.latestProductsSection}>
                 <div className={styles.sectionTitle}>Latest Products</div>
                 <InfiniteScrollComponent />
+            </div>
+            <div className={styles.becomeSellerSection}>
+                <Row>
+                    <Col className={styles.textSection}>
+                        <p>Share your products with hundreds of online customers</p>
+                        <Link to="/sell">Become a Seller -{'>'}</Link>
+                    </Col>
+                    <Col>
+                        <img src='https://i.ibb.co/Zx0sCqW/Sample-image.png' alt=""></img>
+                    </Col>
+                </Row>
             </div>
         </div>
     );
