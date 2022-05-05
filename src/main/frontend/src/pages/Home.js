6d43972176d8aeb5592900b5933ce708ea9dab2c
admirkrilasevic <admirkrilasevic@gmail.com>
2022-05-05 12:23:45 +0200

    mobile responsiveness home sections

--- src/main/frontend/src/pages/Home.js
+++ src/main/frontend/src/pages/Home.js
@@ -17,14 +17,27 @@ function Home(){
         setCategories(popularCategories);
     }, []);
 
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        window.addEventListener('resize', () => setWidth(window.innerWidth));
+    }, []);
+
     return(
         <div>
+            {width >= 768 ?
             <div className={styles.heroSection}>
                 <Row className={styles.center}>
                     <Col><ReactLogo className={styles.logo} /></Col>
                     <Col><HeroImage className={styles.image}/></Col>
                 </Row>
-            </div>
+            </div> :
+            <div className={styles.heroSectionMobile}>
+                <Col className={styles.center}>
+                    <Row><ReactLogo className={styles.logo} /></Row>
+                    <Row><HeroImage className={styles.image}/></Row>
+                </Col>
+            </div>}
             <div className={styles.pageSection}>
                 <div className={styles.sectionTitle}>Browse by Popular Categories</div>
                 <div className="container-fluid">
@@ -43,6 +56,7 @@ function Home(){
                     </Row>
                 </div>
             </div>
+            { width >= 768 ? 
             <div className={styles.becomeSellerSection}>
                 <Row>
                     <Col className={styles.textSection}>
@@ -53,7 +67,18 @@ function Home(){
                         <img src='https://i.ibb.co/Zx0sCqW/Sample-image.png' alt=""></img>
                     </Col>
                 </Row>
-            </div>
+            </div> :
+            <div className={styles.becomeSellerSectionMobile}>
+                <Col>
+                    <Row className={styles.textSection}>
+                        <p>Share your products with hundreds of online customers</p>
+                        <Link to="/sell" className={styles.becomeSellerButtonMobile}>Become a Seller -{'>'}</Link>
+                    </Row>
+                    <Row>
+                        <img src='https://i.ibb.co/Zx0sCqW/Sample-image.png' alt=""></img>
+                    </Row>
+                </Col>
+            </div>}
             <div className={styles.pageSection}>
                 <div className={styles.sectionTitle}>Latest Products</div>
                 <InfiniteScrollComponent />
