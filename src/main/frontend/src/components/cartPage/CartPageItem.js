d43959d171e98d963da3061c902428f1df87b8f4
admirkrilasevic <admirkrilasevic@gmail.com>
2022-05-09 11:39:57 +0200

    cart page responsiveness

--- src/main/frontend/src/components/cartPage/CartPageItem.js
+++ src/main/frontend/src/components/cartPage/CartPageItem.js
@@ -9,28 +9,27 @@ function CartPageItem({ id, photo, name, price, selectedQuantity }) {
   const dispatch = useDispatch();
   
   return (
-    <Container>
-      <Row className={styles.itemContainer}>
-        <Col xs={5}>
-          <Link to={`/items/${id}`}>
-            <img className={styles.photo} src={photo.split(",")[0]} alt="" />
-          </Link>
-        </Col>
-        <Col xs={2}>
-          <Link to={`/items/${id}`} className={styles.itemLink}>
-            <h3 className={styles.title}>{name}</h3>
-          </Link> 
-        </Col>
-        <Col className={styles.price} xs={2}>
-          ${price}
-        </Col>
-        <Col xs={3}>
-          Quantity:
-          <span className={styles.quantity}> {selectedQuantity}</span>
-          <span className={styles.removeButton} onClick={() => dispatch(remove(id))}>x</span>
-        </Col>
-      </Row>
-    </Container>
+    <Row className={styles.itemContainer} >
+      <Col>
+        <Link to={`/items/${id}`}>
+          <img className={styles.photo} src={photo.split(",")[0]} alt="" />
+        </Link>
+      </Col>
+      <Col>
+        <Link to={`/items/${id}`} className={styles.itemLink}>
+          <h3 className={styles.title}>{name}</h3>
+        </Link> 
+      </Col>
+      <Col className={styles.price}>
+        ${price}
+      </Col>
+      <Col>
+        <span className={styles.quantity}>{selectedQuantity}</span>
+      </Col>
+      <Col>
+        <span className={styles.removeButton} onClick={() => dispatch(remove(id))}>REMOVE</span>
+      </Col>
+    </Row>
   );
 }
 
--- src/main/frontend/src/pages/Cart.js
+++ src/main/frontend/src/pages/Cart.js
@@ -28,51 +28,53 @@ function Cart() {
     }, [cartItems]);
 
     return(
+        cartItems.length > 0 ?
         <Container className={"no-gutters"}>
-            {cartItems.length > 0 ?
             <Row>
                 <h3 className={styles.cartTitle}>My Cart</h3>
-                <Col className={styles.itemsContainer} xs={9}>
-                    <Container className={"no-gutters"}>
-                        <Row className={styles.tableTitles}>
-                            <Col xs={5}>
-                                Image
-                            </Col>
-                            <Col xs={2}>
-                                Name
-                            </Col>
-                            <Col className={styles.price} xs={2}>
-                                Price
-                            </Col>
-                            <Col xs={3}> 
-                                Quantity
-                            </Col>
-                        </Row>
-                    </Container>
-                    {cartItems.map((item) => (
-                        <CartPageItem 
-                            key={item.id}
-                            photo={item.photos}
-                            name={item.name}
-                            price={item.price}
-                            selectedQuantity={item.selectedQuantity}
-                        />
-                    ))}
-                    <button onClick={() => handleClear()} className={styles.emptyCartButton}>EMPTY CART</button>
-                </Col>
-                <Col className={styles.subtotalContainer}>
-                    <span>
-                        <span className={styles.subtotalTitle}>Subtotal: </span>
-                        ${itemTotals.reduce((previous, item) => previous + item.total, 0)}
-                    </span>
-                    { itemTotals.length > 0 && <Link className={styles.checkoutButton} to={'/checkout'}>PROCEED TO CHECKOUT</Link> }
-                </Col>
-            </Row> :
-            <Row>
-                <h3 className={styles.cartTitle}>My Cart</h3>
-                <span className={styles.emptyCart}>The cart is currently empty</span>
-            </Row> }
-        </Container>
+            </Row>
+            <Row className={styles.itemsContainer}>
+                <Container className={"no-gutters"}>
+                    <Row className={styles.tableTitles}>
+                        <Col>
+                            Image
+                        </Col>
+                        <Col>
+                            Name
+                        </Col>
+                        <Col className={styles.price}>
+                            Price
+                        </Col>
+                        <Col> 
+                            Quantity
+                        </Col>
+                        <Col>
+                        </Col>
+                    </Row>
+                </Container>
+                {cartItems.map((item) => (
+                    <CartPageItem 
+                        key={item.id}
+                        photo={item.photos}
+                        name={item.name}
+                        price={item.price}
+                        selectedQuantity={item.selectedQuantity}
+                    />
+                ))}
+                <button onClick={() => handleClear()} className={styles.emptyCartButton}>EMPTY CART</button>
+            </Row>
+            <Row className={styles.subtotalContainer}>
+                <span>
+                    <span className={styles.subtotalTitle}>Subtotal: </span>
+                    ${itemTotals.reduce((previous, item) => previous + item.total, 0)}
+                </span>
+                { itemTotals.length > 0 && <Link className={styles.checkoutButton} to={'/checkout'}>PROCEED TO CHECKOUT</Link> }
+            </Row>
+        </Container> :
+        <Row>
+            <h3 className={styles.cartTitle}>My Cart</h3>
+            <span className={styles.emptyCart}>The cart is currently empty</span>
+        </Row> 
     );
 }
 
