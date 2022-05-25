940f5990be911a89165ca5b672deb7a6f395052f
admirkrilasevic <admirkrilasevic@gmail.com>
2022-05-25 22:33:01 +0200

    list view image fix

--- src/main/frontend/src/components/shopPage/ListItem.js
+++ src/main/frontend/src/components/shopPage/ListItem.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function ListItem(props) {
     return (
       <div className={styles.itemContainer}>
-        <Link to={`/items/${props.id}`}><img className={styles.photo} src={props.photo} alt="" /></Link>
+        <Link to={`/items/${props.id}`}><img className={styles.photo} src={props.photo.split(",")[0]} alt="" /></Link>
         <div className={styles.itemInfoContainer}>
             <Link to={`/items/${props.id}`} className={styles.itemLink}><h3 className={styles.title}>{props.name}</h3></Link> 
             <p className={styles.description}>{props.description}</p>
