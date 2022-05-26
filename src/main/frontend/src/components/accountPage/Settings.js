2c0eafbbce5fe2cdfaaaf239703f1657603c5c25
admirkrilasevic <admirkrilasevic@gmail.com>
2022-05-26 21:32:31 +0200

    deactivation section

--- src/main/frontend/src/components/accountPage/Settings.js
+++ src/main/frontend/src/components/accountPage/Settings.js
@@ -1,7 +1,32 @@
-function Settings(){
-    return(
-        <div>
-            <h3>Settings</h3>
+import styles from "./Settings.module.css";
+import UserService from "../../utils/UserService";
+import AuthService from "../../utils/AuthService";
+import { useContext } from "react";
+import { AuthContext } from "../../utils/AuthContext";
+
+const Settings = () => {
+
+    const { setToken } = useContext(AuthContext);
+
+    const deactivateAccount = async () => {
+        const user = AuthService.getCurrentUser();
+        await UserService.deactivate(user.token);
+        setToken(false);
+        AuthService.logout();
+        window.location.replace("/home");
+    }
+
+    return (
+        <div className={styles.settingsContainer}>
+            <div className={styles.settingsSection}>
+                <div className={styles.sectionHeader}>
+                    Account
+                </div>
+                <div className={styles.sectionContent}>
+                    Do you want to deactivate your account?
+                    <button className={styles.deactivateButton} onClick={() => deactivateAccount()}>DEACTIVATE</button>
+                </div>
+            </div>
         </div>
     );
 }
